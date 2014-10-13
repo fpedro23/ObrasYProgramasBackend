1ccc2baff460a7bae5b0a00cc4b8cc9e1fb1739c
Pedro Contreras <fpedro23@gmail.com>
2014-10-12 21:36:13 -0500

    Agregadas las clases

---
+++ WEB-INF/classes/modelObrasYProgramas/ReporteDependenciaPrograma.java
@@ -0,0 +1,33 @@
+package modelObrasYProgramas;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Pedro on 11/10/14.
+ */
+public class ReporteDependenciaPrograma extends ReporteDependencia {
+
+    private String anio;
+
+
+    public ReporteDependenciaPrograma(ResultSet tr) {
+        super(tr);
+
+        try {
+            this.setAnio(tr.getString("anioPrograma"));
+
+        } catch (SQLException sqle) {
+            System.out.println(sqle);
+
+        }
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+}
